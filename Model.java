0c26cb7e22d8ed4e631a03fcc66fe98b37281d09
v.fedorov <v.fedorov@estesis.tech>
2020-11-11 12:01:23 +0700

    add Model

---
+++ Model.java
@@ -0,0 +1,26 @@
+class Model {
+
+    private int a;
+
+    public Model() {
+    }
+
+    public Model(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Model{" +
+                "a=" + a +
+                '}';
+    }
+}
